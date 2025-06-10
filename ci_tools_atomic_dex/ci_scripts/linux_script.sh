ceef630763b8e96d7965b0dd8eaf850927cabd28
cipig <cipianon@gmail.com>
2025-06-10 23:33:02 +0000

    system Qt

--- ci_tools_atomic_dex/ci_scripts/linux_script.sh
+++ ci_tools_atomic_dex/ci_scripts/linux_script.sh
@@ -28,6 +28,12 @@ sudo apt-get install build-essential \
                     libfuse2 \
                     git -y
 
+# Qt
+sudo apt-get install qtbase5-dev -y
+
+# Qt WebEngine
+sudo apt-get install libxcursor-dev libxcomposite-dev libxdamage-dev libxrandr-dev libxtst-dev libxss-dev libdbus-1-dev libevent-dev libfontconfig1-dev libcap-dev libpulse-dev libudev-dev libpci-dev libnss3-dev libasound2-dev libegl1-mesa-dev gperf bison nodejs -y
+
 sudo apt-get update
 sudo apt-get install clang llvm libc++abi-11-dev libc++-11-dev -y
 #export CXXFLAGS=-stdlib=libc++
