1da00b82ab3b4ec46556254aeb3ad857e23905e4
cipig <cipianon@gmail.com>
2025-06-11 02:33:56 +0000

    ubuntu-latest

--- ci_tools_atomic_dex/ci_scripts/linux_script.sh
+++ ci_tools_atomic_dex/ci_scripts/linux_script.sh
@@ -29,7 +29,7 @@ sudo apt-get install build-essential \
 
 # Qt
 sudo apt-get install qtbase5-dev qtdeclarative5-dev qttools5-dev libqt5svg5-dev libqt5charts5-dev qtwebengine5-dev qtquickcontrols2-5-dev -y
-sudo apt-get install libxcursor-dev libxcomposite-dev libxdamage-dev libxrandr-dev libxtst-dev libxss-dev libdbus-1-dev libevent-dev libfontconfig1-dev libcap-dev libudev-dev libpci-dev libnss3 libnss3-dev libasound2-dev libegl1-mesa-dev gperf bison nodejs -y
+sudo apt-get install libxcursor-dev libxcomposite-dev libxdamage-dev libxrandr-dev libxtst-dev libxss-dev libdbus-1-dev libevent-dev libfontconfig1-dev libcap-dev libudev-dev libpci-dev libnss3-dev libasound2-dev libegl1-mesa-dev gperf bison nodejs -y
 
 # clang
 sudo apt-get install clang llvm -y
