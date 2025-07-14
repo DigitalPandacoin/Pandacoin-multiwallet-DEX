01cc2e90fa0f0e78ff196b05138b1c7a02824226
cipig <cipianon@gmail.com>
2025-07-14 13:31:52 +0000

    jammy-backports

--- ci_tools_atomic_dex/ci_scripts/linux_script.sh
+++ ci_tools_atomic_dex/ci_scripts/linux_script.sh
@@ -24,12 +24,12 @@ sudo apt-get install build-essential \
                     libxcb-render-util0-dev \
                     libxcb-xinerama0 \
                     libfuse2 \
-                    git -y
+                    git -t jammy-backports -y
 
 # Qt
-sudo apt-get install qtbase5-dev qtdeclarative5-dev qttools5-dev libqt5svg5-dev libqt5charts5-dev qtwebengine5-dev qtquickcontrols2-5-dev -y
-sudo apt-get install qml-module-qtquick-controls qml-module-qtquick-controls2 qml-module-qtquick-dialogs qml-module-qtquick-extras qml-module-qtquick-layouts qml-module-qtquick-shapes qml-module-qt-labs-settings qml-module-qt-labs-platform qml-module-qtwebengine qml-module-qtcharts qml-module-qtgraphicaleffects -y
-sudo apt-get install libxcursor-dev libxcomposite-dev libxdamage-dev libxrandr-dev libxtst-dev libxss-dev libdbus-1-dev libevent-dev libfontconfig1-dev libcap-dev libudev-dev libpci-dev libnss3-dev libasound2-dev libegl1-mesa-dev gperf bison nodejs -y
+sudo apt-get install qtbase5-dev qtdeclarative5-dev qttools5-dev libqt5svg5-dev libqt5charts5-dev qtwebengine5-dev qtquickcontrols2-5-dev -t jammy-backports -y
+sudo apt-get install qml-module-qtquick-controls qml-module-qtquick-controls2 qml-module-qtquick-dialogs qml-module-qtquick-extras qml-module-qtquick-layouts qml-module-qtquick-shapes qml-module-qt-labs-settings qml-module-qt-labs-platform qml-module-qtwebengine qml-module-qtcharts qml-module-qtgraphicaleffects -t jammy-backports -y
+sudo apt-get install libxcursor-dev libxcomposite-dev libxdamage-dev libxrandr-dev libxtst-dev libxss-dev libdbus-1-dev libevent-dev libfontconfig1-dev libcap-dev libudev-dev libpci-dev libnss3-dev libasound2-dev libegl1-mesa-dev gperf bison nodejs -t jammy-backports -y
 
 #sudo apt-get dist-upgrade -t jammy-backports -y
 
