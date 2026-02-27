ca4c81306ed48bce9d18b8f8ab0d5c8936d33136
Flow <cipianon@gmail.com>
2026-02-27 02:43:32 +0000

    threadpool::initialize_with_threads already set by lib

--- src/app/main.prerequisites.hpp
+++ src/app/main.prerequisites.hpp
@@ -353,10 +353,12 @@ handle_settings(QSettings& settings)
 inline int
 run_app(int argc, char** argv)
 {
-#ifndef _WIN32
-    using namespace crossplat;
-    threadpool::initialize_with_threads(60);
-#endif
+// Exception caught: the cpprestsdk threadpool has already been initialized
+// because it was already initialized by the library
+//#ifndef _WIN32
+//    using namespace crossplat;
+//    threadpool::initialize_with_threads(60);
+//#endif
 #if !defined(ATOMICDEX_HOT_RELOAD)
     SPDLOG_DEBUG("Installing qt_message_handler");
     qInstallMessageHandler(&qt_message_handler);
