1b76f9367a9ad10a7b7f14a2e9f4238cbd64b808
Flow <cipianon@gmail.com>
2026-03-04 10:47:37 +0000

    scoop from GH action

--- ci_tools_atomic_dex/ci_scripts/windows_script.ps1
+++ ci_tools_atomic_dex/ci_scripts/windows_script.ps1
@@ -1,18 +1,5 @@
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 
-iwr -useb 'https://raw.githubusercontent.com/scoopinstaller/install/master/install.ps1' -outfile 'install.ps1'
-.\install.ps1 -RunAsAdmin
-
-#Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh') -RunAsAdmin
-scoop install llvm --global
-scoop install ninja --global
-scoop install git --global
-scoop install 7zip  --global
-scoop cache rm 7zip
-scoop cache rm git
-scoop cache rm ninja
-scoop cache rm llvm
-
 $Env:QT_INSTALL_CMAKE_PATH = "C:\Qt\$Env:QT_VERSION\msvc2019_64"
 $Env:QT_ROOT = "C:\Qt"
 
