732de72c7e51e1a415e1f6c8ee721fcd16ea1e2d
Flow <cipianon@gmail.com>
2026-03-09 14:51:50 +0000

    debug, layout

--- src/core/atomicdex/api/kdf/kdf.client.cpp
+++ src/core/atomicdex/api/kdf/kdf.client.cpp
@@ -164,20 +164,20 @@ namespace atomic_dex::kdf
     pplx::task<web::http::http_response>
     kdf_client::async_rpc_batch_standalone(nlohmann::json batch_array)
     {
-        spdlog::stopwatch sw; using namespace std::chrono;
         try
         {
+            spdlog::stopwatch sw; using namespace std::chrono;
             web::http::http_request request;
             request.set_method(web::http::methods::POST);
             request.set_body(batch_array.dump());
             auto resp = generate_client().request(request, m_token_source.get_token());
+            if (sw.elapsed().count() > 0.03) { SPDLOG_DEBUG("Time elapsed in kdf_client::async_rpc_batch_standalone for coin {} and method {}: {}", batch_array[0].at("coin"), batch_array[0].at("method"), duration_cast<milliseconds>(sw.elapsed())); }
             return resp;
         }
         catch (const std::exception& error)
         {
             SPDLOG_ERROR("exception in kdf_client::async_rpc_batch_standalone: {}", error.what());
         }
-        if (sw.elapsed().count() > 0.03) { SPDLOG_DEBUG("Time elapsed in kdf_client::async_rpc_batch_standalone for coin {} and method {}: {}", batch_array[0].at("coin"), batch_array[0].at("method"), duration_cast<milliseconds>(sw.elapsed())); }
     }
 
     template <rpc Rpc>
